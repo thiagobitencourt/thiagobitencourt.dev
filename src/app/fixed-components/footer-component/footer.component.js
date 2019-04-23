832111d20dfa3ce3a8e89740fed8f900a3a96d65
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2019-04-23 07:02:27 -0300

    Add footer styles

--- src/app/fixed-components/footer-component/footer.component.js
+++ src/app/fixed-components/footer-component/footer.component.js
@@ -1,4 +1,5 @@
 import template from './footer.component.html';
+import './footer.component.scss';
 
 export default function footerComponent(element, data = {}) {
     element.innerHTML = template();
