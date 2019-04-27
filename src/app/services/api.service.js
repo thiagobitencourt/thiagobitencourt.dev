c9a56ff0e68514453eb0a09f4ae09f9ca7e8e9c0
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2019-04-27 18:34:17 -0300

    Remove mock integration

--- src/app/services/api.service.js
+++ src/app/services/api.service.js
@@ -6,12 +6,12 @@ const mapData = result => result.data || {};
 
 export default class ApiService {
     static getArticles(...params) {
-        return Promise.resolve(articles);
+        return Promise.resolve([]);
         // return instance.get('/article', { params }).then(mapData);
     }
 
     static getSingleArticle(id) {
-        return Promise.resolve(getArticle(id));
+        return Promise.resolve({});
         // return instance.get('/article', { params: { id } }).then(mapData);
     }
 }
